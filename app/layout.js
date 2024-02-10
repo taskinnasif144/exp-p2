ed29236119270c9d7c53e3021f6ceccc9c2d5ae8
Dewan Nasif <taskinnasif@gmail.com>
2024-02-10 18:56:33 +0600

    second commit

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} box-border bg-[#202020] font-poppins relative`}>
+      <body className={`${inter.className}`}>
         <Navbar />
         {children}
         {/* <LoginRegisterController /> */}
